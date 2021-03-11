f81b76e8fd0a7f6a5458647606d56c1313203fad
France900 <francesco.em.livio@gmail.com>
2021-03-11 20:34:44 +0100

    finito (piu carino)

--- main.js
+++ main.js
@@ -1,61 +1,67 @@
 $(document).ready(function () {
-
+  // slider()
 })
 
 var arrowLeft = $(".fa-angle-left");
 var arrowRight = $(".fa-angle-right");
 // var clickedElement = $(this);
 
- arrowLeft.click(function () {
-   var activeImage = $(".active");
-   activeImage.removeClass("active").prev().addClass('active');
-   if (activeImage.hasClass("first") && arrowRight.click(function (){})) {
-     $(".first").removeClass("active");
-     $(".last").addClass("active");
-   }
- })
+// function slider() {
+//   arrowLeft.click(function () {
+//     var activeImage = $(".active");
+//     activeImage.removeClass("active").prev().addClass('active');
+//     if (activeImage.hasClass("first") && arrowRight.click(function (){})) {
+//       $(".first").removeClass("active");
+//       $(".last").addClass("active");
+//     }
+//   })
+//
+//   arrowRight.click(function () {
+//     var activeImage = $(".active");
+//     activeImage.removeClass("active").next().addClass('active');
+//     if (activeImage.hasClass("last") && arrowRight.click(function (){})) {
+//       $(".last").removeClass("active");
+//       $(".first").addClass("active");
+//     }
+//   })
+// }
 
- arrowRight.click(function () {
-   var activeImage = $(".active");
-   activeImage.removeClass("active").next().addClass('active');
-   if (activeImage.hasClass("last") && arrowRight.click(function (){})) {
-     $(".last").removeClass("active");
-     $(".first").addClass("active");
-   }
- })
 
 
-
-
- 
-//  arrowRight.click(function () {
-//    var activeImage = $(".active");
-//    activeImage.removeClass("active");
-//    if ($("img.active").hasClass("last")) {
-//      $(".first").addClass("active");
-//      // $(".last").removeClass("active");
-//    } else {
-//      $("img.active").next("img").addClass("active")
-//    }
-//     //es variabile.show() (per i menu a tendina)
+// mia prima idea
+//
+// arrowRight.click(function () {
+//   if ($(".first").hasClass("active")) {
+//     $(".first").removeClass("active").next().addClass("active")
+//   } else {
+//     $(".first").next().hasClass("active").removeClass("active").next().addClass("active");
+//   }
 // })
 
 
-
-
-
-
-
-
-//  arrowRight.click(function () {
-//    console.log("ahahah");
-//    if ($(".first").hasClass("active")) {
-//      console.log("ehehehehehe");
-//      $(".first").removeClass("active").next().addClass("active")
-//    } else {
-//      var ciccio = $(".first").next().hasClass("active").removeClass("active")
-//      ciccio.next().addClass("active");
-//    }
-//     //es variabile.show() (per i menu a tendina)
+// mia prima idea funziona quasi
+//
+// arrowRight.click(function () {
+//   if ($(".first").hasClass("active")) {
+//     $(".first").removeClass("active").next().addClass("active")
+//   } else if (!$(".first").hasClass("active")) {
+//     $(".active").removeClass("active").next().addClass("active");
+//   } else if ($(".last").hasClass("active")) {
+//     console.log(ahahahahaha);
+//     $(".last").removeClass("active");
+//     $(".first").addClass("active");
+//   }
 // })
-// var activeImage = $('.active');
+//
+//
+// arrowLeft.click(function () {
+//   if ($(".last").hasClass("active")) {
+//     $(".first").removeClass("active").next().addClass("active")
+//   } else if (!$(".first").hasClass("active")) {
+//     $(".active").removeClass("active").next().addClass("active");
+//   } else if ($(".last").hasClass("active")) {
+//     console.log(ahahahahaha);
+//     $(".last").removeClass("active");
+//     $(".first").addClass("active");
+//   }
+// })
