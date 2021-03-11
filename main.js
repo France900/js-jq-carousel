785b3a4808b86bdc9b1488f80a29148d30760a66
France900 <francesco.em.livio@gmail.com>
2021-03-11 18:18:41 +0100

    il paradosso

--- main.js
+++ main.js
@@ -12,15 +12,43 @@ arrowLeft.click(function () {
    //es variabile.show() (per i menu a tendina)
  })
 
+//  arrowRight.click(function () {
+//    var activeImage = $(".active");
+//    activeImage.removeClass("active");
+//    if ($("img.active").hasClass("last")) {
+//      $(".first").addClass("active");
+//      // $(".last").removeClass("active");
+//    } else {
+//      $("img.active").next("img").addClass("active")
+//    }
+//     //es variabile.show() (per i menu a tendina)
+// })
 
 
- arrowRight.click(function () {
-   console.log("ahahah");
-   if ($(".first").hasClass("active")) {
-     console.log("ehehehehehe");
-     $(".first").removeClass("active").next().addClass("active")
-   } else if ($(".first").siblings().hasClass("active")) {
-     $(".first").siblings().removeClass("active").next().addClass("active")
-   }
-    //es variabile.show() (per i menu a tendina)
-  })
+
+
+arrowRight.click(function () {
+  var activeImage = $(".active");
+  activeImage.removeClass("active").next().addClass('active');
+  if (activeImage.hasClass("last") && arrowRight.click(function (){})) {
+    $(".last").removeClass("active");
+    $(".first").addClass("active");
+  }
+   //es variabile.show() (per i menu a tendina)
+})
+
+
+
+
+//  arrowRight.click(function () {
+//    console.log("ahahah");
+//    if ($(".first").hasClass("active")) {
+//      console.log("ehehehehehe");
+//      $(".first").removeClass("active").next().addClass("active")
+//    } else {
+//      var ciccio = $(".first").next().hasClass("active").removeClass("active")
+//      ciccio.next().addClass("active");
+//    }
+//     //es variabile.show() (per i menu a tendina)
+// })
+// var activeImage = $('.active');
